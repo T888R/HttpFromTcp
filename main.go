25773d5d6ca33a51ecacf35035364bb341ed082e
T8 <108910965+T888R@users.noreply.github.com>
2025-03-19 17:31:38 -0600

    implemented goroutine functionality for reading

--- main.go
+++ main.go
@@ -9,39 +9,51 @@ import (
 )
 
 func main() {
-	file, err := os.Open("messages.txt")
 
+	file, err := os.Open("messages.txt")
 	if err != nil {
 		fmt.Println("Could not open message file", err)
-		os.Exit(1)
 	}
 
-	lineContents := ""
+	linesChan := getLinesChannel(file)
 
-	for {
-		buf := make([]byte, 8, 8)
-		n, err := file.Read(buf)
-		if err != nil {
-			// fmt.Println("Error reading:", err)
-			if lineContents != "" {
-				fmt.Printf("read: %s\n", lineContents)
-				lineContents = ""
-			}
-			if errors.Is(err, io.EOF) {
+	for line := range linesChan {
+		fmt.Println("read:", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+
+	lines := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		lineContents := ""
+		for {
+			buf := make([]byte, 8, 8)
+			n, err := f.Read(buf)
+			if err != nil {
+				// fmt.Println("Error reading:", err)
+				if lineContents != "" {
+					// fmt.Printf("read: %s\n", lineContents)
+					lines <- lineContents
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
 				break
 			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
+			str := string(buf[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", lineContents, parts[i])
+				lineContents = ""
+			}
+			lineContents += parts[len(parts)-1]
 		}
+	}()
 
-		str := string(buf[:n])
-		parts := strings.Split(str, "\n")
-
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", lineContents, parts[i])
-			lineContents = ""
-		}
-		lineContents += parts[len(parts)-1]
-		// fmt.Println("read:", str)
-	}
+	return lines
 }
