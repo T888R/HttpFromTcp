82ceb51546429b5b31ac41f5ea2e6407de1b16d3
T8 <108910965+T888R@users.noreply.github.com>
2025-03-28 10:58:01 -0600

    added request line reading in main

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"errors"
+	"httpfromtcp/internal/request"
 	// "bufio"
 	"fmt"
-	"io"
+	// "io"
 	"log"
 	"net"
 	// "time"
 	// "os"
-	"strings"
 )
 
 const port = ":42069"
@@ -29,42 +28,13 @@ func main() {
 		}
 		fmt.Println("Accepted connection from", connection.RemoteAddr())
 
-		msgChan := getLinesChannel(connection)
-
-		for line := range msgChan {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(connection)
+		if err != nil {
+			log.Fatalf("error: %s\n", err.Error())
 		}
+
+		fmt.Printf("Request line:\n - Method: %s\n - Target: %s\n - Version: %s\n", req.RequestLine.Method, req.RequestLine.RequestTarget, req.RequestLine.HttpVersion)
+
 		fmt.Println("Connection to ", connection.RemoteAddr(), "closed")
 	}
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-	go func() {
-		defer f.Close()
-		defer close(lines)
-		currentLineContents := ""
-		for {
-			b := make([]byte, 8, 8)
-			n, err := f.Read(b)
-			if err != nil {
-				if currentLineContents != "" {
-					lines <- currentLineContents
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				return
-			}
-			str := string(b[:n])
-			parts := strings.Split(str, "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
-				currentLineContents = ""
-			}
-			currentLineContents += parts[len(parts)-1]
-		}
-	}()
-	return lines
 }
